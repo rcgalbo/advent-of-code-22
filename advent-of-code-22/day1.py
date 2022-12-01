af4dd5014e11be0a98cc5234dd04b435181abc07
rcgalbo <rcgalbo@gmail.com>
2022-12-01 15:44:04 -0500

    base for code,  documentatioon, and tests

---
+++ advent-of-code-22/day1.py
@@ -0,0 +1,38 @@
+# Advent Of Code 
+# Day 1: Calorie Counting
+
+# Sample data
+ex_dat = '''
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+'''
+
+# Elf 1: 1000+2000+3000=6000
+# Elf 2: 4000
+# Elf 3: 5000+6000=11000
+# Elf 4: 7000+8000+9000=24000
+# Elf 5: 1000
+
+# Part 1:
+# Need to find maximum calories carried
+# In our EX this is 24000 Calories
+
+
+def parse_data(data_string):
+    '''
+    Parse data in string format
+    to list format
+    '''
+    elf_calories = data_string.split('\n\n')
