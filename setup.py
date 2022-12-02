64b8a0be87063dd5bc9d59a262807101641176a7
rcgalbo <rcgalbo@gmail.com>
2022-12-01 23:23:53 -0500

    add packaging

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="adventOfCode22",
+    version="1.0.0",
+    author="rcgalbo",
+    author_email="rcgalbo@gmail.com",
+    description="Solutions for advent of code 2022",
+    url="https://github.com/rcgalbo/advent-of-code-22",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
---
+++ tests/test_day1.py
@@ -0,0 +1,22 @@
+from adventOfCode22 import parse_data
+
+# Sample data
+ex_dat = '''
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+'''
+
+def test_parse_data():
+    assert parse_data(ex_dat) == [6000, 4000, 11000, 24000, 10000]
